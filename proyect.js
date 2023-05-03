caba262f67f4008f54272077713a0802aabd1461
Emanuel Ruiz <emahrui@hotmail.com>
2023-05-02 21:20:51 -0300

    Actualizacion para segunda entrega.

--- proyect.js
+++ proyect.js
@@ -1,62 +1,144 @@
+import fs from 'fs';
+
 class ProductManager {
-    constructor(products = []) {
-        this.products = products;
-        this.incrementId = 0;
+    constructor() {
+        this.path = 'productos.json';
+        this.currentId = 1;
+
+        this.getProducts().then((productos) => {
+            if (productos.length > 0) {
+                this.currentId = productos[productos.length - 1].id + 1;
+            }
+        });
     }
 
-    addProduct(title, description, price, thumbnail, code, stock) {
+    async addProduct(product) {
+        let productos = await this.getProducts()
+        const requiredFields = ['title', 'description', 'price', 'thumbnail', 'code', 'stock'];
 
-        if (!title || !description || !price || !thumbnail || !code || !stock) {
-            console.log('Todos los campos son obligatorios.');
+        // Validar que todos los campos requeridos estén presentes
+        for (const field of requiredFields) {
+            if (!product[field]) {
+                console.error(`El campo ${field} es obligatorio`);
+                return;
+            }
+        }
+
+        // Validar que no se repita el código del producto
+        const existingProduct = productos.find((p) => p.code === product.code);
+        if (existingProduct) {
+            console.error(`El producto con el código ${product.code} ya existe`);
             return;
         }
-        if (this.products.some((p) => p.code === code)) {
-            console.log('El código ya está en uso.');
-            return;
-        }
 
-        const product = {
-            id: ++this.incrementId,
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock,
-        };
-        this.products.push(product);
-        return product;
+        // Agregar el producto al arreglo y asignarle un id autoincrementable
+        const newProduct = { ...product, id: this.currentId };
+        productos.push(newProduct);
+        this.currentId++;
+        await fs.promises.writeFile(this.path, JSON.stringify(productos))
+
+        console.log('Producto agregado:', newProduct);
     }
 
-    getProducts() {
-        console.log(this.products);
+    async getProducts() {
+        let archivo = await fs.promises.readFile(this.path)
+        let productos = JSON.parse(archivo)
+        return productos
     }
 
-    getProductById(id) {
-        const product = this.products.find((p) => p.id === id);
-        if (!product) {
-            console.error('Producto no encontrado.');
-        } else {
-            console.log('Producto encontrado:', product);
+    async getProductById(id) {
+        let productos = await this.getProducts()
+        let productoEncontrado = productos.find(producto => producto.id == id)
+        return productoEncontrado ? productoEncontrado : "No encontrado";
+    }
+
+    async updateProduct(id, updatedProduct) {
+        let productos = await this.getProducts()
+        let index = productos.findIndex(producto => producto.id == id)
+        if (index == -1) {
+            console.error(`El producto con el id ${id} no existe`)
+            return
         }
+        productos[index] = { ...productos[index], ...updatedProduct }
+        await fs.promises.writeFile(this.path, JSON.stringify(productos))
+        console.log(`Producto con el id ${id} actualizado`)
+    }
+
+    async deleteProduct(id) {
+        let productos = await this.getProducts()
+        let index = productos.findIndex(producto => producto.id == id)
+        if (index == -1) {
+            console.error(`El producto con el id ${id} no existe`)
+            return
+        }
+        productos.splice(index, 1)
+        await fs.promises.writeFile(this.path, JSON.stringify(productos))
+        console.log(`Producto con el id ${id} eliminado`)
     }
 }
 
 const productManager = new ProductManager();
-//Productos agregados correctamente.
-productManager.addProduct('producto 1', 'descripción', 10.40, 'ruta/de/imagen', 'ABC123', 5);
-productManager.addProduct('producto 2', 'descripción', 19.50, 'ruta/de/imagen', 'DEF456', 10);
+await productManager.updateProduct(2, { price: 30 });
 
-//Ejemplo de code igual.
-productManager.addProduct('producto 3', 'descripción', 20.82, 'ruta/de/imagen', 'DEF456', 20);
-//Ejemplo de propiedad faltante.
-productManager.addProduct('producto 4', 'descripción', 12.79, 'ruta/de/imagen', 'DAF026');
+// Productos agregados correctamente.
+await productManager.addProduct({
+    title: 'Producto 1',
+    description: 'Descripción del producto 1',
+    price: 10.99,
+    thumbnail: 'ruta/de/imagen',
+    code: 'ABC123',
+    stock: 5,
+});
+await productManager.addProduct({
+    title: 'Producto 2',
+    description: 'Descripción del producto 2',
+    price: 19.99,
+    thumbnail: 'ruta/de/imagen',
+    code: 'DEF456',
+    stock: 10,
+});
+await productManager.addProduct({
+    title: 'Producto 3',
+    description: 'Descripción del producto 3',
+    price: 32.99,
+    thumbnail: 'ruta/de/imagen',
+    code: 'DED496',
+    stock: 90,
+});
 
-//Lista.
-productManager.getProducts()
-//Ejemplo de encontrado.
-productManager.getProductById(1);
+// Ejemplo de code igual.
+await productManager.addProduct({
+    title: 'Producto 4',
+    description: 'Descripción del producto 4',
+    price: 15.99,
+    thumbnail: 'ruta/de/imagen',
+    code: 'ABC123',
+    stock: 12,
+});
+
+// Ejemplo de propiedad faltante.
+await productManager.addProduct({
+    title: 'Producto 5',
+    description: 'Descripción del producto 5',
+    price: 600,
+    code: 'OUD132',
+    stock: 22,
+});
+
+// Lista
+console.log(await productManager.getProducts());
+
+// Ejemplo de encontrado.
+await productManager.getProductById(1);
+
 //Ejemplo de no encontrado.
-productManager.getProductById(4);
+await productManager.getProductById(4);
 
-//NOTA: No sabia si había que tener imágenes en el proyecto ya que por lo que entendí por ahora ejecutaríamos el programa por consola.
+//Modifica un producto.
+await productManager.updateProduct(2, { price: 30 });
+
+//Elimina un producto.
+await productManager.deleteProduct(3); // elimina el producto con id = 3
+
+// Nueva lista.
+console.log(await productManager.getProducts());
